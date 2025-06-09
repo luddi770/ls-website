3b4f7018dad809206f86d479c29affb1d92c9043
luddi770 <133671112+luddi770@users.noreply.github.com>
2025-06-09 23:04:34 +0200

    Change the Projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,11 +2,11 @@ export const projects = [
     {
         title: 'Portfolio Website',
         description: 'A personal portfolio site built with React and Tailwind CSS.',
-        link: 'https://github.com/yourname/portfolio'
+        link: 'https://github.com/luddi770/ls-website'
     },
     {
-        title: 'Weather App',
-        description: 'Fetches weather data using OpenWeatherMap API and displays it with animations.',
-        link: 'https://github.com/yourname/weather-app'
+        title: 'TextEditor',
+        description: 'A simple Texteditor i wrote in C. In the future i want to change it into a Vim Clone',
+        link: 'https://github.com/luddi770/TextEditor_C'
     }
 ];
